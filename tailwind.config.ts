63f8a1910647b1bd717c78bcb074fdb8784ac847
HeRa Khan <78750278+HOTLancerX@users.noreply.github.com>
2024-07-02 22:58:39 +0600

    4

---
+++ tailwind.config.ts
@@ -0,0 +1,25 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '0.5rem',
+        md: '1.5rem',
+      },
+    },
+    extend: {
+      colors: {
+        'main': '#124d9d',
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
